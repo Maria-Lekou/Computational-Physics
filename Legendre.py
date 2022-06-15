3c540dc3ae93daa7c2ebea69c509415108431f2d
Maria Lekou <70920643+Maria-Lekou@users.noreply.github.com>
2022-06-16 01:02:44 +0300

    Create Legendre.py

--- Legendre.py
+++ Legendre.py
@@ -26,3 +26,47 @@ def normPm1(el,m):
     return y[0]
     
 y[1] = 1
+
+ """"
+Η f είναι πίνακας που μέσω αυτής θα υπολογίζουμε
+το δεξί μέλος του συστήματος σε κάθε βήμα.
+Το πρώτο στοιχείο είναι το dP/dx=y[1] και το δεύτερο 
+d^2/dx^2=f()
+"""
+
+
+"Υπολογισμός Plm από runge-Kutta για x=xi, (-1,1)"
+
+ 
+for m in range(0,2):
+    plt.figure()
+    for l in range(m,5):
+        i = -1
+        if m==0:
+            y[0]=normPm0(l,m)
+        if m==1:
+            y[0]=normPm1(l,m)
+        el=l
+        def f(xi,yi):
+            rhs=np.zeros(2) # Declare array dimension
+            rhs[0]=yi[1]
+            rhs[1]=2*xi*yi[1]/(1-xi**2)-(el*(el+1)-m**2/(1-xi**2))*yi[0]/(1-xi**2)
+            return rhs
+        for xi in np.arange (-1+step , 1-step , step ):
+            i=i+1 
+            x[i]=xi
+            """
+            N=2 γιατί έχουμε σύστημα οπότε είναι σαν να
+            καλούμε την RK 2 φορές
+            """
+            y=rk4Algor(xi,step,2,y,f) 
+            Plm[i]=y[0]
+        if max(abs(Plm))>1:
+            Plm=Plm/max(abs(Plm))
+        plt.plot(x,Plm,label=r'$l=$'+str(l))
+        plt.grid(True)
+        plt.title(r'Legendre Polynomials, $P_l^%i(x)$'%m)
+        plt.xlabel('x')
+        plt.ylabel('$\mathbf{P_l^m(x)}$')
+        plt.legend(loc='best')
+    plt.show()
